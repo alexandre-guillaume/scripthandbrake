f349dc6b8baf3ee600c06832debc0ec57284cefb
alex <alexandrelepetitdu49@hotmail.fr>
2016-03-20 12:02:30 +0100

    Added files via upload

---
+++ scripthandbrakev2.py
@@ -0,0 +1,37 @@
+# -*-coding:Latin-1 -*
+import random
+import os 
+import subprocess
+import sys , getopt
+from os import listdir
+from os.path import isfile, join
+# utilisation python scripthandbrakev2.py "chemin de fichier" 
+# le script va faire un listing des fichier dans le dossier et va convertir les video en x265 
+def main(argv):
+	handbrakecli = "HandbrakeCLI.exe"
+	option = " -f mkv -P -e x265 -q 20 -B 160"
+	inputfile = sys.argv[1]
+	print 'nombre argument', len(sys.argv)
+	if len(sys.argv) > 2:
+		outputfile = sys.argv[2]
+	else:
+		outputfile = inputfile
+
+
+	onlyfiles = [f for f in listdir(inputfile) if isfile(join(inputfile, f))]
+	print 'liste de fichiers', onlyfiles
+	for f in onlyfiles:
+		comandein =   inputfile + '\\' + f 
+		comandeout =   outputfile + '\\' + f + '.mkv' 
+		commandeligne = handbrakecli + " -i " + comandein + " -o " + comandeout + option
+		#subprocess.Popen lance les X commande ou a utiliser sur cluster ou cpu avec beaucoup de coeur
+		#subprocess.Popen(commandeligne)
+		proc = subprocess.Popen(commandeligne)
+		proc.wait()
+		print 'un fichier terminer'
+	print 'terminer'
+if __name__ == "__main__":
+    main(sys.argv)
+
+#os.system("pause")
+
