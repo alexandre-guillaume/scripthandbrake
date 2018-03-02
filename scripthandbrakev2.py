4d2304a95987dcc184cd1d0c76f9049f72178ff8
alex <alexandrelepetitdu49@hotmail.fr>
2018-03-02 20:43:52 +0100

    Update scripthandbrakev2.py
    ajouter tu deusieme argument dans le script pour rename

--- scripthandbrakev2.py
+++ scripthandbrakev2.py
@@ -6,7 +6,10 @@ import sys , getopt
 from os import listdir
 from os.path import isfile, join
 # utilisation python scripthandbrakev2.py "chemin de fichier" 
-# le script va faire un listing des fichier dans le dossier et va convertir les video en x265 
+# le script va faire un listing des fichier dans le dossier et va convertir les video en x265
+
+#ATTENTION il ne faut pas d'espace dans le chemin vers les fichier n'y dans les fichier, nulle part
+
 # https://handbrake.fr/docs/en/latest/cli/cli-guide.html
 def main(argv):
 	handbrakecli = "HandbrakeCLI.exe"
@@ -14,26 +17,34 @@ def main(argv):
 	option = " -f mkv -P -e x265 -q 21 -E copy"
 	inputfile = sys.argv[1]
 	print 'nombre argument', len(sys.argv)
-	if len(sys.argv) > 2:
-		outputfile = sys.argv[2]
-	else:
-		outputfile = inputfile
+	outputfile = inputfile
+	nouveauNom = ''
+	if len(sys.argv) > 3:
+		outputfile = sys.argv[3]
+	elif len(sys.argv) > 2:
+		nouveauNom = sys.argv[2]
 
 
 	onlyfiles = [f for f in listdir(inputfile) if isfile(join(inputfile, f))]
 	print 'liste de fichiers', onlyfiles
 	for f in onlyfiles:
-		comandein =   inputfile + '\\' + f 
-		comandeout =   outputfile + '\\' + f + '.mkv' 
+		indice = 1
+		comandein =   inputfile + '\\' + f
+		#test si nouveauNom
+		if len(nouveauNom) == 0: 
+			comandeout =   outputfile + '\\' + f + '.mkv'
+		else:
+			comandeout =   outputfile + '\\' + nouveauNom + '_' + str(indice) + '.mkv'
 		commandeligne = handbrakecli + " -i " + comandein + " -o " + comandeout + option
 		#subprocess.Popen lance les X commande ou a utiliser sur cluster ou cpu avec beaucoup de coeur
 		#subprocess.Popen(commandeligne)
 		proc = subprocess.Popen(commandeligne)
 		proc.wait()
+		indice +=1
+		#pas de i++ en python
 		print 'un fichier terminer'
 	print 'terminer'
 if __name__ == "__main__":
     main(sys.argv)
 
 #os.system("pause")
-
